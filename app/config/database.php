9cb95c6e87d8908a8c2561e17adc07fc2b755ebc
Micar-Arguelles <micarshanearguelles@gmail.com>
2025-09-13 15:58:28 +0800

    update

--- app/config/database.php
+++ app/config/database.php
@@ -63,7 +63,7 @@ $database['main'] = array(
     'port'		=> '3306',
     'username'	=> 'sql12798460',
     'password'	=> 'gzQeM8igJD',
-    'database'	=> 'mockdata',
+    'database'	=> 'sql12798460',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
