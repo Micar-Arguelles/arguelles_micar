58d7983b5aefbc7a8c766702ee1fbb74f34b1f06
Micar-Arguelles <micarshanearguelles@gmail.com>
2025-09-19 15:54:18 +0800

    update

--- app/models/UsersModel.php
+++ app/models/UsersModel.php
@@ -10,24 +10,27 @@ class UsersModel extends Model {
         parent::__construct();
     }
 
+    // Kunin lahat ng users with pagination + search
     public function get_users($limit, $offset, $search = '')
     {
         $builder = $this->db->table($this->table);
 
-        if(!empty($search)){
+        if (!empty($search)) {
             $builder->like('first_name', $search);
             $builder->or_like('last_name', $search);
             $builder->or_like('email', $search);
         }
 
-        return $builder->limit($limit, $offset)->get_all();
+        $builder->limit($limit, $offset);
+        return $builder->get_all();
     }
 
+    // Bilangin total users for pagination
     public function count_users($search = '')
     {
         $builder = $this->db->table($this->table);
 
-        if(!empty($search)){
+        if (!empty($search)) {
             $builder->like('first_name', $search);
             $builder->or_like('last_name', $search);
             $builder->or_like('email', $search);
